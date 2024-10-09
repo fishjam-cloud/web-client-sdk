f0e254b475205f02ac13f69e50bce0320d0c4f6a
Adrian Czerwiec <adrian.czerwiec@swmansion.com>
2024-10-09 10:27:35 +0200

    expose isMuted and deviceError

---
+++ packages/react-client/src/hooks/devices/useCamera.ts
@@ -0,0 +1,38 @@
+import { Device } from "../../types/public";
+import { useDeviceManager } from "../deviceManagers/useDeviceManager";
+import { useFishjamContext } from "../useFishjamContext";
+
+/**
+ *
+ * @category Devices
+ */
+export function useCamera(): Device {
+  const { videoTrackManager, videoDeviceManagerRef } = useFishjamContext();
+  const { deviceState, status } = useDeviceManager(videoDeviceManagerRef.current);
+
+  const { currentTrack, ...trackManager } = videoTrackManager;
+
+  const stream = deviceState.media?.stream ?? null;
+  const currentMiddleware = deviceState.currentMiddleware ?? null;
+  const isStreaming = Boolean(currentTrack?.stream);
+  const track = stream?.getAudioTracks()[0] ?? null;
+  const trackId = currentTrack?.trackId ?? null;
+  const devices = deviceState.devices ?? [];
+  const activeDevice = deviceState.media?.deviceInfo ?? null;
+  const isMuted = !deviceState.media?.enabled;
+  const deviceError = deviceState.error ?? null;
+
+  return {
+    ...trackManager,
+    currentMiddleware,
+    status,
+    stream,
+    track,
+    devices,
+    activeDevice,
+    deviceError,
+    isMuted,
+    isStreaming,
+    trackId,
+  };
+}
---
+++ packages/react-client/src/hooks/devices/useMicrophone.ts
@@ -0,0 +1,40 @@
+import { AudioDevice } from "../../types/public";
+import { useDeviceManager } from "../deviceManagers/useDeviceManager";
+import { useFishjamContext } from "../useFishjamContext";
+
+/**
+ *
+ * @category Devices
+ */
+export function useMicrophone(): AudioDevice {
+  const { audioTrackManager, audioDeviceManagerRef } = useFishjamContext();
+  const { deviceState, status } = useDeviceManager(audioDeviceManagerRef.current);
+
+  const { currentTrack, ...trackManager } = audioTrackManager;
+
+  const stream = deviceState.media?.stream ?? null;
+  const currentMiddleware = deviceState.currentMiddleware ?? null;
+  const isStreaming = Boolean(currentTrack?.stream);
+  const track = stream?.getAudioTracks()[0] ?? null;
+  const trackId = currentTrack?.trackId ?? null;
+  const devices = deviceState.devices ?? [];
+  const activeDevice = deviceState.media?.deviceInfo ?? null;
+  const isAudioPlaying = currentTrack?.vadStatus === "speech";
+  const isMuted = !deviceState.media?.enabled;
+  const deviceError = deviceState.error ?? null;
+
+  return {
+    ...trackManager,
+    currentMiddleware,
+    status,
+    stream,
+    track,
+    isStreaming,
+    trackId,
+    devices,
+    activeDevice,
+    isMuted,
+    deviceError,
+    isAudioPlaying,
+  };
+}
