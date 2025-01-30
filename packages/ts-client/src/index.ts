ea10841f0f7f93a226ee7d22e73783bf5e1daa65
Milosz Filimowski <milosz.filimowski@swmansion.com>
2025-01-30 13:56:50 +0100

    lint fix

--- packages/ts-client/src/index.ts
+++ packages/ts-client/src/index.ts
@@ -1,6 +1,6 @@
-export { type AuthErrorReason, AUTH_ERROR_REASONS, isAuthError } from './auth.js';
+export { AUTH_ERROR_REASONS, type AuthErrorReason, isAuthError } from './auth.js';
 export { FishjamClient } from './FishjamClient';
-export { type JoinErrorReason, JOIN_ERRORS, isJoinError } from './guards';
+export { isJoinError, JOIN_ERRORS, type JoinErrorReason } from './guards';
 export type { ReconnectConfig, ReconnectionStatus } from './reconnection';
 export type {
   Component,
