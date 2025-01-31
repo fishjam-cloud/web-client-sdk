c0ef35d2fd2a8b38f00ff013a594f4e5d74c5cb9
Miłosz Filimowski <MiloszFilimowski@users.noreply.github.com>
2025-01-31 08:42:46 +0100

    [FCE-1112]: Better information on connected peer limit reached (#267)
    ## Description
    
    - Always log a warning with the reason when the socket is closed
    - Emit join error when a known error occurres.
    
    ## Motivation and Context
    
    - Socket errors were silently ignored and resulted in an unknown state. 
    
    ## Types of changes
    
    - [x] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to
          not work as expected)

--- packages/ts-client/src/guards.ts
+++ packages/ts-client/src/guards.ts
@@ -12,3 +12,10 @@ export const isComponent = (endpoint: Endpoint): endpoint is Component =>
   endpoint.type === 'file' ||
   endpoint.type === 'rtsp' ||
   endpoint.type === 'sip';
+
+export const JOIN_ERRORS = ['reached peers limit', 'room not found', 'node not found', 'Invalid SDK version'] as const;
+
+export type JoinErrorReason = (typeof JOIN_ERRORS)[number];
+
+export const isJoinError = (error: string): error is JoinErrorReason =>
+  JOIN_ERRORS.some((knownError) => error.trim().toLowerCase().includes(knownError.trim().toLowerCase()));
