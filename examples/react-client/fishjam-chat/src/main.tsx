a295d63e6e1f7ff60699bfcf7e6277d0cc43772b
Adrian Czerwiec <adrian.czerwiec@swmansion.com>
2025-02-21 11:46:11 +0100

    add toast to fishjamchat

--- examples/react-client/fishjam-chat/src/main.tsx
+++ examples/react-client/fishjam-chat/src/main.tsx
@@ -6,12 +6,14 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App.tsx";
 import { BlurProvider } from "./components/BlurToggle.tsx";
+import { Toaster } from "./components/ui/sonner.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <FishjamProvider>
       <BlurProvider>
         <App />
+        <Toaster />
       </BlurProvider>
     </FishjamProvider>
   </React.StrictMode>,
