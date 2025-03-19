53824e2cf16b0fe11cbcebca896dcbd3bb7cb7f5
Adrian Czerwiec <adrian.czerwiec@swmansion.com>
2025-03-19 15:46:15 +0100

    fix ts issue

--- packages/react-client/src/contexts/fishjamState.ts
+++ packages/react-client/src/contexts/fishjamState.ts
@@ -2,4 +2,4 @@ import { createContext } from "react";
 
 import type { FishjamClientState } from "../hooks/internal/useFishjamClientState";
 
-export const FishjamClientStateContext = createContext<FishjamClientState>(null);
+export const FishjamClientStateContext = createContext<FishjamClientState | null>(null);
