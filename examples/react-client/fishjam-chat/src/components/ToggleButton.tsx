05660fc565a308dd81caf039a572dcf57c479558
Adrian Czerwiec <adrian.czerwiec@swmansion.com>
2025-04-03 15:10:23 +0200

    format

--- examples/react-client/fishjam-chat/src/components/ToggleButton.tsx
+++ examples/react-client/fishjam-chat/src/components/ToggleButton.tsx
@@ -3,7 +3,7 @@ import type { FC } from "react";
 
 import { cn } from "@/lib/utils";
 
-import { Button,type ButtonProps } from "./ui/button";
+import { Button, type ButtonProps } from "./ui/button";
 
 type Props = {
   isOn: boolean;
