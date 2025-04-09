f36a2060e6ef9f700f90863a5c62a51c920751c5
Adrian Czerwiec <adrian.czerwiec@swmansion.com>
2025-04-09 16:09:35 +0200

    tidy react broadcast example

--- examples/react-client/broadcast/vite.config.ts
+++ examples/react-client/broadcast/vite.config.ts
@@ -1,15 +1,11 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import checker from "vite-plugin-checker";
-// import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    // https://vitejs.dev/config/server-options.html#server-host
-    // true - listen on all addresses, including LAN and public addresses
     host: true,
-    // https: true,
     port: 3007,
   },
   plugins: [
@@ -20,6 +16,5 @@ export default defineConfig({
         lintCommand: "eslint --ext .ts,.tsx",
       },
     }),
-    // mkcert(),
   ],
 });
