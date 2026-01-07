8c39600f86708a6b438853b786f83ba5651982da
Milosz Filimowski <milosz.filimowski@swmansion.com>
2026-01-07 13:17:19 +0100

    fix react resolution in examples

--- packages/mobile-client/src/webrtc-polyfill.ts
+++ packages/mobile-client/src/webrtc-polyfill.ts
@@ -1,6 +1,7 @@
-import { registerGlobals } from '@fishjam-cloud/react-native-webrtc';
 // @ts-ignore - event-target-shim types not properly exported via package.json exports
 import { EventTarget } from 'event-target-shim';
+
+import { registerGlobals } from '@fishjam-cloud/react-native-webrtc';
 
 import { RTCPeerConnection } from './overrides/RTCPeerConnection';
 
