5eb3a957f51484f3c7a5c2af3f0407091ac5e0e5
Miłosz Filimowski <MiloszFilimowski@users.noreply.github.com>
2026-01-08 15:55:25 +0100

    FCE-2467: Implement proper crypto polyfil (#442)
    ## Description
    
    - use `react-native-get-random-values` as `getRandomValues`.
    
    ## Motivation and Context
    
    - We had a temporary implmenation. 
    
    ## Documentation impact
    
    - [ ] Documentation update required
    - [ ] Documentation updated [in another PR](_)
    - [x] No documentation update required
    
    ## Types of changes
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to
          not work as expected)

--- packages/mobile-client/src/webrtc-polyfill.ts
+++ packages/mobile-client/src/webrtc-polyfill.ts
@@ -1,3 +1,5 @@
+import 'react-native-get-random-values';
+
 // @ts-ignore - event-target-shim types not properly exported via package.json exports
 import { EventTarget } from 'event-target-shim';
 
@@ -9,24 +11,6 @@ import { LocalStoragePolyfill } from './polyfills/local-storage';
 const registerGlobalsPolyfill = () => {
   (global as unknown as { EventTarget: typeof EventTarget }).EventTarget = EventTarget;
   (global as unknown as { localStorage: typeof localStorage }).localStorage = new LocalStoragePolyfill();
-  (global as unknown as { crypto: { getRandomValues: (array: Uint8Array) => void } }).crypto = {
-    getRandomValues: function (array: Uint8Array) {
-      for (let i = 0; i < array.length; i++) {
-        array[i] = Math.floor(Math.random() * 256);
-      }
-      return array;
-    },
-  };
-
-  // TODO: FCE-2467 Implement proper polyfill for crypto
-  (global as unknown as { crypto: { getRandomValues: (array: Uint8Array) => void } }).crypto.getRandomValues =
-    function (array: Uint8Array) {
-      for (let i = 0; i < array.length; i++) {
-        array[i] = Math.floor(Math.random() * 256);
-      }
-      return array;
-    };
-
   registerGlobals();
   // Custom overrides
   (globalThis.RTCPeerConnection as unknown as typeof RTCPeerConnection) = RTCPeerConnection;
