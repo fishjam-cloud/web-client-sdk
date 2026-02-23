ddc38685c4bda273ef0ba1678c60cf7ed79c66c8
Adrian Czerwiec <adrian.czerwiec@swmansion.com>
2026-02-23 13:19:52 +0100

    fix fishjam url

--- e2e-tests/livestream-client/src/main.tsx
+++ e2e-tests/livestream-client/src/main.tsx
@@ -2,12 +2,12 @@ import { FishjamProvider } from "@fishjam-cloud/react-client";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import { FISHJAM_ID } from "../config";
+import { FISHJAM_URL } from "../config";
 import { App } from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <FishjamProvider fishjamId={FISHJAM_ID}>
+    <FishjamProvider fishjamId={FISHJAM_URL}>
       <App />
     </FishjamProvider>
   </React.StrictMode>,
