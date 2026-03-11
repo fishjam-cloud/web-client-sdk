c736de45437c4280e87d7c29ac62f2017dffb456
Paweł Gadomski <xpfk.098@gmail.com>
2026-03-11 10:06:00 +0100

    Fix prettier config for mobile-client

--- examples/mobile-client/text-chat/App.tsx
+++ examples/mobile-client/text-chat/App.tsx
@@ -1,8 +1,8 @@
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { SafeAreaProvider } from "react-native-safe-area-context";
-import { FishjamProvider } from "@fishjam-cloud/react-native-client";
-import RootNavigation from "./navigation/RootNavigation";
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { FishjamProvider } from '@fishjam-cloud/react-native-client';
+import RootNavigation from './navigation/RootNavigation';
 
 const App = () => {
   return (
