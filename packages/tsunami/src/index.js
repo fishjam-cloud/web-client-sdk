a8cdb56fbd6f6cfd08f9b09fc6ad87f835188841
Milosz Filimowski <milosz.filimowski@swmansion.com>
2026-08-03 14:45:49 +0200

    remove stray tsc-emitted artifacts from src dirs

--- packages/tsunami/src/index.js
+++
@@ -1,9 +0,0 @@
-export { classifyDeviceError, DeviceError, DeviceNotFoundError, DeviceOverconstrainedError, DevicePermissionDeniedError, UnknownDeviceError, } from "./devices/errors";
-export { LocalStorageDevicePersistence } from "./devices/LocalStorageDevicePersistence";
-export { WebDeviceManager } from "./devices/WebDeviceManager";
-export { FishjamError } from "./errors/FishjamError";
-export { ClientDisposedError, DeviceManagerMissingError } from "./errors/lifecycleErrors";
-export { FishjamClient } from "./FishjamClient";
-export { createInitialClientState, } from "./state/clientState";
-export { StateStore } from "./state/StateStore";
-export * from "@fishjam-cloud/ts-client";
